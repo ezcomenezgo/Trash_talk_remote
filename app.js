9ea917fe146bd81b59c5dde18d4d559c9f93ee02
Claire <s3310507057@gmail.com>
2020-12-19 17:40:43 +0800

    feat: add partial template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // includes related packages in project
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -8,8 +9,19 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
+// setting body parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // setting routes
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
